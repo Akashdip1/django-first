73c771d3b44f8216f27d9dcbf512e272bf94310f
Akashdip1 <akashdip.baral@gmail.com>
2024-08-01 18:17:23 +0530

    Working till updateView

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from .views import (
     PostDetailView, 
     PostListView, 
-    PostCreateView
+    PostCreateView,
+    PostUpdateView
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),  # defining path to home page of the blog
-    path('post/int:<pk>', PostDetailView.as_view(), name='post-detail'),  
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),  
     path('post/new/', PostCreateView.as_view(), name='post-create'),  
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),  
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView)
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post 
 
 
@@ -28,7 +32,16 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
