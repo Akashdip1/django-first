cab80f344a011908fbaf65ae75789445c7dd8712
Akashdip1 <akashdip.baral@gmail.com>
2024-08-01 18:01:11 +0530

    Working till createView

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -10,4 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.title)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from .views import (
+    PostDetailView, 
+    PostListView, 
+    PostCreateView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),  # defining path to home page of the blog
+    path('', PostListView.as_view(), name='blog-home'),  # defining path to home page of the blog
+    path('post/int:<pk>', PostDetailView.as_view(), name='post-detail'),  
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  
     path('about/', views.about, name='blog-about'),
 ]
