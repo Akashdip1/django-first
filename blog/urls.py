a83046f90d9f59b7cd734c715bcecb264ab3c72f
Akashdip1 <akashdip.baral@gmail.com>
2024-08-07 11:52:47 +0530

    Added posts from posts.json

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+    PostDeleteView,
     PostDetailView, 
     PostListView, 
     PostCreateView,
@@ -11,6 +12,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),  # defining path to home page of the blog
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),  
     path('post/new/', PostCreateView.as_view(), name='post-create'),  
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),  
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  
     path('about/', views.about, name='blog-about'),
 ]
